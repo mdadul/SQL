8e0e051836a70506721a333d4527c67e4b9609f5
Emdadul Islam <58410798+mdadul@users.noreply.github.com>
2022-01-08 08:56:11 +0600

    Update

--- Hakerrank_ReversingTheSelectQueryII.sql
+++ Hakerrank_ReversingTheSelectQueryII.sql
@@ -1,2 +1,5 @@
 SELECT NAME FROM CITY 
 WHERE POPULATION >120000 and CountryCode ='USA';
+
+/* Problem link   : https://www.hackerrank.com/challenges/revising-the-select-query-2/problem?isFullScreen=true */
+
